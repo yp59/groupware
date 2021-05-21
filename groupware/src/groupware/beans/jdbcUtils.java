dc9eb4cfd76278d0f0e7e5bef1d2e9d37b360084
user <user@116.121.145.129>
2021-05-21 16:23:52 +0900

    JdbcUtils 연결 설정 변경

--- groupware/src/groupware/beans/jdbcUtils.java
+++ groupware/src/groupware/beans/jdbcUtils.java
@@ -3,6 +3,10 @@ package groupware.beans;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 public class jdbcUtils {
 
 	public static Connection con(String USERNAME,String PASSWORD)throws Exception{
@@ -12,6 +16,22 @@ public class jdbcUtils {
 				("jdbc:oracle:thin:@www.sysout.co.kr:1521:xe", USERNAME, PASSWORD);
 		
 		return con;
+	}
+	
+	private static DataSource ds;
+	static {
+		try {
+			Context ctx = new InitialContext();
+			ds=(DataSource)ctx.lookup("java:comp/env/jdbc/oracle");
+		}
+		catch(Exception e) {
+			System.out.println("데이터베이스 연결 실패");
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection getConnection() throws Exception {
+		return ds.getConnection();
 	}
 	
 }
