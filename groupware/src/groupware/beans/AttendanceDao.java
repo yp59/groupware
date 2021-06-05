922338379c20e2620441adf91901f0f290d023f0
ohasis <ohasis@LAPTOP-0AS9OEBG>
2021-06-06 01:00:58 +0900

    데이터 삭제하고 새로운 데이터 다시 삽입

--- groupware/src/groupware/beans/AttendanceDao.java
+++ groupware/src/groupware/beans/AttendanceDao.java
@@ -95,12 +95,14 @@ public class AttendanceDao {
 		ps.setString(1,empNo);
 		
 		ResultSet rs = ps.executeQuery();
+		int sumovertime = 0;
+		
 		if(rs.next()) {
-			return rs.getInt("sumovertime");
+			sumovertime= rs.getInt("sumovertime");
 		}
 		
 		con.close();
-		return 0;
+		return sumovertime;
 	}
 	
 	// 이번달 추가 근무시간 값 가져오기 -> 관리자
